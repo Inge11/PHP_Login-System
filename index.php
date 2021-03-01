b9f69c9aff8fa4d9cca90303f1be9a03a8d47af1
Your Name <robertschuller100@googlemail.com>
2021-03-01 17:47:01 +0100

    anges in index php i#

--- index.php
+++ index.php
@@ -1,1 +1,47 @@
 <?php echo "Whats wrong?"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.17/dist/css/uikit.min.css" />
+</head>
+<body>
+    <div class="uk-section uk-container uk-text -center">
+    <div class="uk-grid-column-small uk-grid-row-large" uk-grid>
+
+    <form class="uk-form-stacked">
+
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-text">email</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="email" placeholder="oo@web.de">
+        </div>
+    </div>
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-text">Password</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="passsword" placeholder="Password">
+        </div>
+    </div>
+
+    
+    <div class="uk-margin">
+       
+        
+        <button class="uk-button uk-button-default" type="submit">Login</button>
+    </div>
+    
+
+</form>
+    </div>
+    </div>
+    <!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.6.17/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.6.17/dist/js/uikit-icons.min.js"></script>
+</body>
+</html>
